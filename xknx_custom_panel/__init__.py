0bddf7006a8fb7ce52e27a5d98f60be82c464b9f
marvin-w <me@marvin-wichmann.de>
2022-04-27 22:38:32 +0200

    fix(UI): Directly return KNX UI path from init

--- xknx_custom_panel/__init__.py
+++ xknx_custom_panel/__init__.py
@@ -1,1 +1,8 @@
 """Custom panel for the KNX integration."""
+import os
+
+
+def get_knx_ui() -> str:
+    """Get path to KNX UI."""
+    return os.path.dirname(os.path.realpath(__file__)).replace("__init__.py", "knx_ui.js")
+
