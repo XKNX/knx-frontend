f8640f116a8a9f2f1e40f91b8ac2e3cc7eb2daec
marvin-w <me@marvin-wichmann.de>
2022-04-30 11:51:14 +0200

    chore(Libs): Add NgRx dependency and fix tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { KNXPanelComponent } from './knxpanel/knxpanel.component';
+import { createCustomElement } from '@angular/elements';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [KNXPanelComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {
-  constructor(private injector: Injector) {}
+  constructor(private injector: Injector) {
+    const webComponent = createCustomElement(KNXPanelComponent, { injector });
+    customElements.define('knx-panel', webComponent);
+  }
 }
