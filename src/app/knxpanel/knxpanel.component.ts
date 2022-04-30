709b6384c01d9ae9920783b901fd6ed5472e7d51
marvin-w <me@marvin-wichmann.de>
2022-04-30 12:40:37 +0200

    chore(CI): Add ESLint

--- src/app/knxpanel/knxpanel.component.ts
+++ src/app/knxpanel/knxpanel.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HomeAssistant } from 'custom-card-helpers';
 
 @Component({
-  selector: 'app-knxpanel',
+  selector: 'knx-panel',
   templateUrl: './knxpanel.component.html',
   styleUrls: ['./knxpanel.component.scss'],
 })
 export class KNXPanelComponent implements OnInit {
+  @Input()
+  hass!: HomeAssistant;
+
+  @Input()
+  narrow!: boolean;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.hass);
+  }
 }
