b7cf51e4455c78ea6fc118b31c4406464733a89b
Marvin Wichmann <me@marvin-wichmann.de>
2022-08-15 02:09:03 +0200

    Fix view

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
-import { html, TemplateResult } from "lit";
+import "@polymer/app-layout/app-header/app-header";
+import "@polymer/app-layout/app-toolbar/app-toolbar";
+import "@material/mwc-list/mwc-list-item";
+import "@material/mwc-button";
+import "@material/mwc-fab";
+import {css, html, TemplateResult} from "lit";
 import { customElement, property } from "lit/decorators";
 import { applyThemesOnElement } from "../homeassistant-frontend/src/common/dom/apply_themes_on_element";
 import { navigate } from "../homeassistant-frontend/src/common/navigate";
 import { makeDialogManager } from "../homeassistant-frontend/src/dialogs/make-dialog-manager";
 import "../homeassistant-frontend/src/resources/ha-style";
+import "../homeassistant-frontend/src/components/ha-tabs";
+import "../homeassistant-frontend/src/components/ha-menu-button";
+import "../homeassistant-frontend/src/layouts/ha-app-layout";
+import "../homeassistant-frontend/src/layouts/hass-subpage";
 import { HomeAssistant, Route } from "../homeassistant-frontend/src/types";
 import { knxElement } from "./knx";
 import "./knx-router";
 import { LocationChangedEvent } from "./types/navigation";
+import {haStyle} from "../homeassistant-frontend/src/resources/styles";
 
 @customElement("knx-frontend")
 class KnxFrontend extends knxElement {
@@ -42,6 +52,23 @@ class KnxFrontend extends knxElement {
     }
 
     return html`
+      <ha-app-layout>
+        <app-header fixed slot="header">
+          <app-toolbar>
+            <ha-menu-button .hass=${this.hass} .narrow=${this.narrow}></ha-menu-button>
+            <div main-title>KNX UI</div>
+          </app-toolbar>
+          <ha-tabs
+              scrollable
+              attr-for-selected="page-name"
+              .selected=${this.route.path}
+              @iron-activate=${this.handleNavigationEvent}
+          >
+            <paper-tab page-name="/knx/overview"> Overview </paper-tab>
+            <paper-tab page-name="/knx/monitor"> Bus Monitor </paper-tab>
+          </ha-tabs>
+        </app-header>
+      </ha-app-layout>
       <knx-router
         .hass=${this.hass}
         .knx=${this.knx}
@@ -49,6 +76,15 @@ class KnxFrontend extends knxElement {
         .narrow=${this.narrow}
       ></knx-router>
     `;
+  }
+
+  private handleNavigationEvent(event: any) {
+    const path = event.detail.item.getAttribute("page-name");
+    navigate(path, {replace: true});
+  }
+
+  static get styles() {
+    return haStyle;
   }
 
   private _setRoute(ev: LocationChangedEvent): void {
