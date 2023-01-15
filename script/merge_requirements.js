ebca910e961fa00a12ff50fcf4c57495fe3c207a
farmio <farmio@alphart.net>
2023-01-15 01:08:51 +0100

    merge_requirements script update yarn version

--- script/merge_requirements.js
+++ script/merge_requirements.js
@@ -1,21 +1,30 @@
 const fs = require("fs");
+const yaml = require('js-yaml');
 
-let rawcore = fs.readFileSync("./homeassistant-frontend/package.json");
-let rawknx = fs.readFileSync("./package.json");
+let raw_package_core = fs.readFileSync("./homeassistant-frontend/package.json");
+let raw_package_knx = fs.readFileSync("./package.json");
 
-const core = JSON.parse(rawcore);
-const knx = JSON.parse(rawknx);
+
+const package_core = JSON.parse(raw_package_core);
+const package_knx = JSON.parse(raw_package_knx);
 
 fs.writeFileSync(
   "./package.json",
   JSON.stringify(
     {
-      ...knx,
-      resolutions: { ...core.resolutions, ...knx.resolutionsOverride },
-      dependencies: { ...core.dependencies, ...knx.dependenciesOverride },
-      devDependencies: { ...core.devDependencies, ...knx.devDependenciesOverride },
+      ...package_knx,
+      resolutions: { ...package_core.resolutions, ...package_knx.resolutionsOverride },
+      dependencies: { ...package_core.dependencies, ...package_knx.dependenciesOverride },
+      devDependencies: { ...package_core.devDependencies, ...package_knx.devDependenciesOverride },
     },
     null,
     2
   )
 );
+
+
+let yarn_release = fs.readdirSync("./homeassistant-frontend/.yarn/releases/").filter(fn => fn.match(/yarn-\d.*\.cjs/) !== null)
+
+const yarnrc = yaml.load(fs.readFileSync("./.yarnrc.yml", 'utf8'));
+yarnrc.yarnPath = 'homeassistant-frontend/.yarn/releases/' + yarn_release[0]
+fs.writeFileSync("./.yarnrc.yml", yaml.dump(yarnrc));
