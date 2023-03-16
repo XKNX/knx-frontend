7996c7b1e6e6315cf0fcbb1e0366e5a11893a6c1
farmio <farmio@alphart.net>
2023-03-16 20:44:51 +0100

    fix imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,13 @@
-"""KNX Frontend"""
+"""KNX Frontend."""
 from .constants import DEV, FILE_HASH
 
 
-def locate_dir():
+def locate_dir() -> str:
     """Return the location of the frontend files."""
     return __path__[0]
 
 
-def get_build_id():
+def get_build_id() -> str:
     """Get the KNX panel build id."""
     if DEV:
         return "dev"
