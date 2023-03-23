60530591c2057bc70e9c91265c6b3f964b8e75d7
farmio <farmio@alphart.net>
2023-03-23 08:37:54 +0100

    migrate `.yarnrc.yml` from HA frontend submodule

--- script/merge_requirements.js
+++ script/merge_requirements.js
@@ -1,24 +1,28 @@
 const fs = require("fs");
 
-let rawcore = fs.readFileSync("./homeassistant-frontend/package.json");
-let rawknx = fs.readFileSync("./package.json");
+const rawPackageCore = fs.readFileSync("./homeassistant-frontend/package.json");
+const rawPackageKnx = fs.readFileSync("./package.json");
 
-const core = JSON.parse(rawcore);
-const knx = JSON.parse(rawknx);
+const packageCore = JSON.parse(rawPackageCore);
+const packageKnx = JSON.parse(rawPackageKnx);
 
 fs.writeFileSync(
   "./package.json",
   JSON.stringify(
     {
-      ...knx,
-      resolutions: { ...core.resolutions, ...knx.resolutionsOverride },
-      dependencies: { ...core.dependencies, ...knx.dependenciesOverride },
+      ...packageKnx,
+      resolutions: { ...packageCore.resolutions, ...packageKnx.resolutionsOverride },
+      dependencies: { ...packageCore.dependencies, ...packageKnx.dependenciesOverride },
       devDependencies: {
-        ...core.devDependencies,
-        ...knx.devDependenciesOverride,
+        ...packageCore.devDependencies,
+        ...packageKnx.devDependenciesOverride,
       },
     },
     null,
     2
   )
 );
+
+const yarnRcCore = fs.readFileSync("./homeassistant-frontend/.yarnrc.yml", 'utf8');
+const yarnRcKnx = yarnRcCore.replace(/\.yarn\//g, "homeassistant-frontend/.yarn/")
+fs.writeFileSync("./.yarnrc.yml", yarnRcKnx);
