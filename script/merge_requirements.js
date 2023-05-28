9e62bd3b3d1bb2c6b5c8f2f4b7e52300fe005f0f
farmio <farmio@alphart.net>
2023-05-28 21:59:18 +0200

    update merge_requirements.js to use `import`

--- script/merge_requirements.js
+++ script/merge_requirements.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+import fs from "fs";
 
 const rawPackageCore = fs.readFileSync("./homeassistant-frontend/package.json");
 const rawPackageKnx = fs.readFileSync("./package.json");
