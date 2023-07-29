c677362d0f662596da7054364c8a5e048a4ee618
farmio <farmio@alphart.net>
2023-07-29 21:27:27 +0200

    update build scripts

--- build-scripts/webpack.cjs
+++ build-scripts/webpack.cjs
@@ -40,7 +40,7 @@ const createWebpackConfig = ({
   const ignorePackages = bundle.ignorePackages({ latestBuild });
   return {
     mode: isProdBuild ? "production" : "development",
-    target: ["web", latestBuild ? "es2017" : "es5"],
+    target: `browserslist:${latestBuild ? "modern" : "legacy"}`,
     devtool: isProdBuild
       ? "cheap-module-source-map"
       : "eval-cheap-module-source-map",
@@ -78,6 +78,13 @@ const createWebpackConfig = ({
       ],
       moduleIds: isProdBuild && !isStatsBuild ? "deterministic" : "named",
       chunkIds: isProdBuild && !isStatsBuild ? "deterministic" : "named",
+      splitChunks: {
+        // Disable splitting for web workers with ESM output
+        // Imports of external chunks are broken
+        chunks: latestBuild
+          ? (chunk) => !chunk.canBeInitial() && !/^.+-worker$/.test(chunk.name)
+          : undefined,
+      },
     },
     plugins: [
       new WebpackBar({ fancy: !isProdBuild }),
@@ -170,6 +177,7 @@ const createWebpackConfig = ({
       })],
     },
     output: {
+      module: latestBuild,
       filename: ({ chunk }) => {
         if (!isProdBuild || isStatsBuild || dontHash.has(chunk.name)) {
           return `${chunk.name}-dev.js`;
@@ -184,6 +192,7 @@ const createWebpackConfig = ({
       globalObject: "self",
     },
     experiments: {
+      outputModule: true,
       topLevelAwait: true,
     },
   };
@@ -193,5 +202,6 @@ const createKNXConfig = ({ isProdBuild, latestBuild }) =>
   createWebpackConfig(bundle.config.knx({ isProdBuild, latestBuild }));
 
 module.exports = {
-  createKNXConfig: createKNXConfig,
+  createKNXConfig,
+  createWebpackConfig,
 };
