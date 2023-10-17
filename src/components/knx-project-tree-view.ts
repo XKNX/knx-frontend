f63531acba82dd72f3f493e79bec50ddb1c2c0a7
farmio <farmio@alphart.net>
2023-10-17 21:26:39 +0200

    Add GroupRange selector and filtered GA-DataTable

--- src/components/knx-project-tree-view.ts
+++ src/components/knx-project-tree-view.ts
@@ -1,10 +1,54 @@
 import { css, CSSResultGroup, html, LitElement, nothing, TemplateResult } from "lit";
-import { customElement, property } from "lit/decorators";
+import { customElement, property, state } from "lit/decorators";
+
+import { fireEvent } from "@ha/common/dom/fire_event";
+
 import { GroupRange, KNXProject } from "../types/websocket";
+import { KNXLogger } from "../tools/knx-logger";
+
+const logger = new KNXLogger("knx-project-tree-view");
+
+declare global {
+  // for fire event
+  interface HASSDomEvents {
+    "knx-group-range-selection-changed": GroupRangeSelectionChangedEvent;
+  }
+}
+
+export interface GroupRangeSelectionChangedEvent {
+  groupAddresses: string[];
+}
+
+interface RangeInfo {
+  selected: boolean;
+  groupAddresses: string[];
+}
 
 @customElement("knx-project-tree-view")
 export class KNXProjectTreeView extends LitElement {
   @property({ reflect: false }) data!: KNXProject;
+
+  @state() _selectedRanges: GroupRange[] = [];
+
+  @state() private _selectableRanges: { [key: string]: RangeInfo } = {};
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    const initSelectableRanges = (data: { [key: string]: GroupRange }) => {
+      Object.entries(data).forEach(([key, groupRange]) => {
+        if (groupRange.group_addresses.length > 0) {
+          this._selectableRanges[key] = {
+            selected: false,
+            groupAddresses: groupRange.group_addresses,
+          };
+        }
+        initSelectableRanges(groupRange.group_ranges);
+      });
+    };
+    initSelectableRanges(this.data.group_ranges);
+    logger.log("ragnes", this._selectableRanges);
+  }
 
   protected render(): TemplateResult {
     return html`<div class="container ha-tree-view">
@@ -12,39 +56,77 @@ export class KNXProjectTreeView extends LitElement {
     </div>`;
   }
 
-  protected _renderGAs(group_range_data: GroupRange): TemplateResult {
-    const tmpl = group_range_data.group_addresses
-      ? group_range_data.group_addresses.map((ga) => {
-          const ga_name = this.data.group_addresses[ga]!.name;
-          return html`<summary class="ga">${ga} - ${ga_name}</summary>`;
-        })
-      : nothing;
-    return html`${tmpl}`;
+  protected _recurseData(data: { [key: string]: GroupRange }): TemplateResult {
+    const childTemplates = Object.entries(data).map(([key, groupRange]) => {
+      const hasSubRange = Object.keys(groupRange.group_ranges).length > 0;
+      const selectable = key in this._selectableRanges;
+
+      const checkbox = selectable
+        ? html`<input
+            type="checkbox"
+            .id=${key}
+            .checked=${this._selectableRanges[key].selected}
+            @change=${this._selectionChanged}
+          />`
+        : nothing;
+      const _rangeName = key + " - " + groupRange.name;
+      const rangeContent = selectable
+        ? html`<label>${checkbox} ${_rangeName}</label>`
+        : html`${_rangeName}`;
+
+      if (hasSubRange) {
+        const subselected = Object.keys(groupRange.group_ranges).reduce(
+          (accumulator, rangeKey) =>
+            rangeKey in this._selectableRanges
+              ? this._selectableRanges[rangeKey].selected
+                ? accumulator + 1
+                : accumulator
+              : accumulator,
+          0,
+        );
+        return html`<details>
+          <summary>${rangeContent} ${subselected ? "(" + subselected + ")" : nothing}</summary>
+          ${this._recurseData(groupRange.group_ranges)}
+        </details>`;
+      }
+      return html`${rangeContent}`;
+    });
+    return html`${childTemplates}`;
   }
 
-  protected _recurseData(data: { [key: string]: GroupRange }): TemplateResult {
-    const childTemplates = Object.keys(data).map(
-      (key) =>
-        html`<details>
-          <summary>${key} - ${data[key].name}</summary>
-          ${data[key].group_ranges ? this._recurseData(data[key].group_ranges) : nothing}
-          ${this._renderGAs(data[key])}
-        </details>`,
+  private _selectionChanged(ev) {
+    const rangeKey = ev.target.id;
+    this._selectableRanges[rangeKey].selected = !this._selectableRanges[rangeKey].selected;
+    this._selectionUpdate();
+    this.requestUpdate();
+  }
+
+  private _selectionUpdate() {
+    const _gaOfSelectedRanges = Object.values(this._selectableRanges).reduce(
+      (result, rangeInfo) =>
+        rangeInfo.selected ? result.concat(rangeInfo.groupAddresses) : result,
+      [] as string[],
     );
-    return html`${childTemplates}`;
+    logger.debug("selection changed", _gaOfSelectedRanges);
+    fireEvent(this, "knx-group-range-selection-changed", { groupAddresses: _gaOfSelectedRanges });
   }
 
   static get styles(): CSSResultGroup {
     return css`
-      details details {
+      details {
         margin-left: 10pt;
         display: block;
       }
-      summary:hover {
+      summary:hover,
+      p:hover {
         background-color: rgba(var(--rgb-primary-text-color), 0.1);
       }
-      summary.ga {
+      summary {
         margin-left: 10pt;
+        display: block;
+      }
+      label {
+        margin-left: 20pt;
         display: block;
       }
     `;
--- src/views/project_explore.ts
+++ src/views/project_explore.ts
@@ -1,21 +1,30 @@
 import { LitElement, TemplateResult, html, css } from "lit";
 import { customElement, property, state } from "lit/decorators";
 
+import { HASSDomEvent } from "@ha/common/dom/fire_event";
 import "@ha/layouts/hass-tabs-subpage";
 import type { PageNavigation } from "@ha/layouts/hass-tabs-subpage";
 import "@ha/components/ha-card";
 import "@ha/components/ha-circular-progress";
+import "@ha/components/ha-expansion-panel";
+import "@ha/components/data-table/ha-data-table";
+import type {
+  DataTableColumnContainer,
+  DataTableRowData,
+} from "@ha/components/data-table/ha-data-table";
+
 import "../components/knx-project-tree-view";
 
 import { compare } from "compare-versions";
 
 import { HomeAssistant, Route } from "@ha/types";
 import { KNX } from "../types/knx";
-import { KNXProjectRespone } from "../types/websocket";
+import type { GroupRangeSelectionChangedEvent } from "../components/knx-project-tree-view";
+import { GroupAddress, KNXProjectRespone } from "../types/websocket";
 import { getKnxProject } from "../services/websocket.service";
 import { KNXLogger } from "../tools/knx-logger";
 
-const logger = new KNXLogger("info");
+const logger = new KNXLogger("knx-project-explore");
 // Minimum XKNXProject Version needed which was used for parsing the ETS Project
 const MIN_XKNXPROJECT_VERSION = "3.3.0";
 
@@ -33,8 +42,39 @@ export class KNXProjectExplore extends LitElement {
 
   @state() private _knxProjectResp: KNXProjectRespone | null = null;
 
+  @state() private _columns: DataTableColumnContainer = {};
+
+  @state() private _visibleGroupAddresses: string[] = [];
+
   protected firstUpdated() {
     this._getKnxProject();
+
+    this._columns = {
+      address: {
+        filterable: true,
+        sortable: true,
+        title: "Address",
+        width: "10%",
+      },
+      text: {
+        filterable: true,
+        sortable: true,
+        title: "Name",
+        width: "40%",
+      },
+      description: {
+        filterable: true,
+        sortable: true,
+        title: "Description",
+        width: "40%",
+      },
+      dpt: {
+        sortable: true,
+        filterable: true,
+        title: "DPT",
+        width: "10%",
+      },
+    };
   }
 
   private _getKnxProject() {
@@ -49,35 +89,74 @@ export class KNXProjectExplore extends LitElement {
     );
   }
 
+  private _groupAddressToRow(groupAddress: GroupAddress, _index: number): DataTableRowData {
+    const dpt = groupAddress.dpt
+      ? groupAddress.dpt.main +
+        (groupAddress.dpt.sub ? "." + groupAddress.dpt.sub.toString().padStart(3, "0") : "")
+      : "";
+    return {
+      address: groupAddress.address,
+      text: groupAddress.name,
+      description: groupAddress.description,
+      dpt: dpt,
+    };
+  }
+
+  private _getRows(visibleGroupAddresses: string[]): DataTableRowData[] {
+    return Object.entries(this._knxProjectResp!.knxproject.group_addresses)
+      .filter(([key, _val]) =>
+        // if none is set, default to show all
+        visibleGroupAddresses.length ? visibleGroupAddresses.includes(key) : true,
+      )
+      .map(([_ga, groupAddress], index) => this._groupAddressToRow(groupAddress, index));
+  }
+
+  private _visibleAddressesChanged(ev: HASSDomEvent<GroupRangeSelectionChangedEvent>) {
+    this._visibleGroupAddresses = ev.detail.groupAddresses;
+  }
+
   private _renderTreeView(): TemplateResult {
+    const filtered = this._getRows(this._visibleGroupAddresses);
     return html`
       ${this._knxProjectResp?.project_loaded
-        ? html` <ha-card
-            class="knx-project-tree"
-            .header=${this.knx.localize("project_explore_tree_view_title")}
-          >
-            <div class="card-content">
-              ${compare(
-                this._knxProjectResp?.knxproject.info.xknxproject_version ?? "0.0.0",
-                MIN_XKNXPROJECT_VERSION,
-                ">=",
-              )
-                ? html`
-                    <knx-project-tree-view
-                      .data=${this._knxProjectResp.knxproject}
-                    ></knx-project-tree-view>
-                  `
-                : html`
-                    <p>${this.knx.localize("project_explore_version_l1")}</p>
-                    <p style="margin-left: 16px;font-weight: bold">
-                      ${this._knxProjectResp?.knxproject.info.xknxproject_version} &lt;
-                      ${MIN_XKNXPROJECT_VERSION}
-                      (${this.knx.localize("project_explore_version_l2")})
-                    </p>
-                    <p>${this.knx.localize("project_explore_version_l3")}</p>
-                  `}
-            </div>
-          </ha-card>`
+        ? html` <ha-expansion-panel
+              class="knx-project-tree"
+              outlined
+              .header=${this.knx.localize("project_explore_tree_view_title")}
+            >
+              <div class="card-content">
+                ${compare(
+                  this._knxProjectResp?.knxproject.info.xknxproject_version ?? "0.0.0",
+                  MIN_XKNXPROJECT_VERSION,
+                  ">=",
+                )
+                  ? html`
+                      <knx-project-tree-view
+                        .data=${this._knxProjectResp.knxproject}
+                        @knx-group-range-selection-changed=${this._visibleAddressesChanged}
+                      ></knx-project-tree-view>
+                    `
+                  : html`
+                      <p>${this.knx.localize("project_explore_version_l1")}</p>
+                      <p style="margin-left: 16px;font-weight: bold">
+                        ${this._knxProjectResp?.knxproject.info.xknxproject_version} &lt;
+                        ${MIN_XKNXPROJECT_VERSION}
+                        (${this.knx.localize("project_explore_version_l2")})
+                      </p>
+                      <p>${this.knx.localize("project_explore_version_l3")}</p>
+                    `}
+              </div>
+            </ha-expansion-panel>
+            <ha-data-table
+              class="ga-table"
+              .hass=${this.hass}
+              .columns=${this._columns}
+              .data=${filtered}
+              .hasFab=${false}
+              .searchLabel=${this.hass.localize("ui.components.data-table.search")}
+              id="index"
+              .clickable=${false}
+            ></ha-data-table>`
         : html` <ha-card class="knx-project-tree" .header=${this.knx.localize("attention")}>
             <div class="card-content">
               <p>${this.knx.localize("project_explore_upload")}</p>
@@ -110,14 +189,18 @@ export class KNXProjectExplore extends LitElement {
     return css`
       .rows {
         display: flex;
-        justify-content: center;
+        /* justify-content: center; */
         flex-direction: column;
+        height: 100%;
       }
 
       .knx-project-tree {
-        margin-right: 8px;
-        margin-left: 8px;
-        margin-top: 8px;
+        margin: 8px;
+        flex: 0;
+      }
+
+      .ga-table {
+        flex: 1;
       }
 
       ha-circular-progress {
