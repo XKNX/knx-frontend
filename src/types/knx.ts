fc8d5430af4721ebe0c247f32ab22724da5dd020
Matthias Alphart <farmio@alphart.net>
2023-11-08 11:48:43 +0100

    Removed unused stuff in router and knxElement (#109)

--- src/types/knx.ts
+++ src/types/knx.ts
@@ -2,11 +2,7 @@ import { ConfigEntry } from "@ha/data/config_entries";
 
 export interface KNX {
   language: string;
-  updates: any[];
-  resources: any[];
-  removed: any[];
   config_entry: ConfigEntry;
-  sections: any;
   localize(string: string, replace?: Record<string, any>): string;
   log: any;
 }
