42bf8a50d671675b2307fcd2214b0db2a5b4f0e1
farmio <farmio@alphart.net>
2025-06-29 16:23:23 +0200

    Remove entity schemas from frontend

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,42 +1,29 @@
 import { mdiToggleSwitchVariant, mdiCheckCircle, mdiWindowShutter } from "@mdi/js";
 import { FALLBACK_DOMAIN_ICONS } from "@ha/data/icons";
-import * as schema from "./schema";
 import type { SupportedPlatform } from "../types/entity_data";
 
 export const SUPPORTED_PLATFORMS = ["switch", "light", "binary_sensor", "cover"] as const;
 
 export interface PlatformInfo {
-  name: string;
   iconPath: string;
   color: string;
-  description?: string; // TODO: remove hardcoded description
-  schema: schema.Section[]; // TODO: remove hardcoded schema
 }
 
 export const platformConstants: Record<SupportedPlatform, PlatformInfo> = {
   binary_sensor: {
-    name: "Binary Sensor",
     iconPath: mdiCheckCircle,
     color: "var(--green-color)",
-    description: "Read-only entity for binary datapoints. Window or door states etc.",
-    schema: schema.binarySensorSchema,
   },
   switch: {
-    name: "Switch",
     iconPath: mdiToggleSwitchVariant,
     color: "var(--blue-color)",
-    description: "The KNX switch platform is used as an interface to switching actuators.",
-    schema: schema.switchSchema,
   },
   light: {
     iconPath: FALLBACK_DOMAIN_ICONS.light,
     color: "var(--amber-color)",
   },
   cover: {
-    name: "Cover",
     iconPath: mdiWindowShutter,
     color: "var(--cyan-color)",
-    description: "The KNX cover platform is used as an interface to shutter actuators.",
-    schema: schema.coverSchema,
   },
 };
